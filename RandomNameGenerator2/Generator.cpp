25f6fb2f809854ed33f002a026fc8a96113c586a
Mark Tasaka <mark.tasaka@gmail.com>
2023-01-28 18:51:17 -0800

    Update 1.01

--- RandomNameGenerator2/Generator.cpp
+++ RandomNameGenerator2/Generator.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include "Generator.h"
+#include "MaleNameGiven.h"
 using namespace std;
 
 bool Generator::isNameMale(int input)
@@ -19,9 +20,26 @@ bool Generator::isNameMale(int input)
 string* Generator::getFirstName(int code, bool isMale, int count)
 {
 	string* firstName = new string[count];
+
+	if (isMale == true)
+	{
+		MaleNameGiven maleName = MaleNameGiven();
+
+		firstName = maleName.getFirstName(code, count);
+	}/*
+	else
+	{
+		FemaleNameGiven femaleName = FemaleNameGiven();
+
+		firstName = femaleName.getFirstName(code, count);
+	}*/
+
+	return firstName;
 }
 
+/*
 string* Generator::getSurname(int code, int count)
 {
+	string* surname = new string[count];
 
-}
+}*/
--- RandomNameGenerator2/main.cpp
+++ RandomNameGenerator2/main.cpp
@@ -3,6 +3,8 @@
 #include <iomanip>
 #include<ctime>
 #include<cstdlib>
+#include "Generator.h"
+#include "MaleNameGiven.h"
 using namespace std;
 
 
@@ -11,5 +13,34 @@ int main()
 {
 
 	srand(time(0));
+
+	bool isMale = true;
+	int numberOfNames = 50;
+	int firstNameSelect = 2;
+
+	cout << "Hello World" << endl;
+
+	Generator test1 = Generator();
+
+	MaleNameGiven testName = MaleNameGiven();
+
+	string* firstName = new string[numberOfNames];
+
+	firstName = testName.getFirstName(firstNameSelect, numberOfNames);
+
+	//string* firstName[];
+	//firstName = test1.getFirstName(firstNameSelect, isMale, numberOfNames);
+
+	cout << firstName[0] << endl;
+
+
+	for (int i = 0; i < numberOfNames; i++)
+	{
+		cout << firstName[i] << " " << endl;
+	}
+
+
+	return 0;
+
 
 }
